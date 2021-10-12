c1d9d65d303186439af7c8198edb737a4e683237
Pranay Jain <jpranay688@gmail.com>
2021-10-12 22:43:25 +0530

    add 3d background boxes

---
+++ 40- 3D Background Boxes/app.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelector("#boxes")
+const btn = document.querySelector("#btn")
+
+createBoxes()
+
+function createBoxes() {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			const box = document.createElement("div")
+			box.classList.add("box")
+			box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+
+			boxes.appendChild(box)
+		}
+	}
+}
+
+btn.addEventListener("click", () => boxes.classList.toggle("big"))
